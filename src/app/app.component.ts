7ad2956407ba97ae8035b746e39f6c6c5faa131d
Jackson Merma <jmermap@unsa.edu.pe>
2021-07-21 16:25:24 -0500

    app angular iniciada y agregando espacios en los componentes

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'my-dream-app';
+  name = "Jackson Fernando Merma Portocarrero";
+  email = "jmermap@unsa.edu.pe";
+  webpage = "http://www.unsa.edu.pe";
+}
