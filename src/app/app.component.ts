00fc0fbe844d1d8529eb55bf34075d4a01a18a83
Jackson Merma <jmermap@unsa.edu.pe>
2021-07-21 17:11:15 -0500

    apps user y hello-world e implementaciones .html .ts .css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,8 +6,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  users=['jack', 'daniela', 'roro'];
+  activate=false;
   title = 'my-dream-app';
-  name = "Jackson Fernando Merma Portocarrero";
-  email = "jmermap@unsa.edu.pe";
-  webpage = "http://www.unsa.edu.pe";
+  name:string;
+  email;
+  webpage:string;
+  hobbies:string[];
+  showHobbies: boolean;
+
+  constructor(){
+    console.log("Constructor working...");
+    this.name = "Jackson Fernando Merma Portocarrero";
+    this.email = "jmermap@unsa.edu.pe";
+    this.webpage = "http://www.unsa.edu.pe";
+    this.hobbies = ["Futbol","Programacion","Music"];
+    this.showHobbies = false;
+  }
+
+  showhobbies(){
+    return this.showHobbies;
+  }
+
+  toggleHobbies(){
+    this.showHobbies = !this.showHobbies;
+  }
+
+  newHobby(hobby:any){
+    this.hobbies.push(hobby.value);
+    hobby.value="";
+    return false;
+  }
 }
