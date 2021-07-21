5a620413544af3c888c8cb6db4961ecffbb33ee7
Jackson Merma <jmermap@unsa.edu.pe>
2021-07-21 17:23:11 -0500

    eliminando usuarios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -41,4 +41,11 @@ export class AppComponent {
   sayHello(){
     alert("hola desde app.component")
   }
+  deleteUser(user:any){
+    for(let i=0;i<this.users.length;i++){
+      if(user==this.users[i]){
+        this.users.splice(i, 1);
+      }      
+    }
+  }
 }
